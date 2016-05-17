9ab4f934ae695aeedea97a7dd13b2657cc364fa2
Matthew Bajorek <bajorek.matthew@gmail.com>
2016-05-17 16:35:50 -0400

    Fixed pdf not loading after loading html file

--- responsive/js/collapse.js
+++ responsive/js/collapse.js
@@ -1,13 +1,22 @@
 $( document ).ready(function() {
 	// Collapse panel
-    $(".panel-collapse").on("shown.bs.collapse", function () {
+    $(".panel-collapse").one("shown.bs.collapse", function () {
     	// Select the name of html and pdf
-    	var name = $(this).children().attr('name');
+    	var panelBody = $(this).children();
+    	var name = panelBody.attr('name');
     	// Load html into section
-		$(this).children().load('articles/' + name + '.html');
-		// Load pdf doc into selection
-		$(this).children().append('<hr><p>Printable PDF</p>')
-		$(this).children().append('<a href="articles/' + name + '.pdf" class="btn btn-primary" target="_blank"><i class="fa fa-file-pdf-o fa-3x"></i></a>');
+		panelBody.load('articles/' + name + '.html', function(responseTxt, statusTxt, xhr){
+	        if(statusTxt == "success")
+	            // Load pdf doc into selection after content is loaded
+				var text = $('<hr><p>Printable PDF</p>');
+				var link = $('<a href="articles/' + name + '.pdf" class="btn" target="_blank">\
+					<i class="fa fa-file-pdf-o fa-3x"></i>\
+					</a>');
+				panelBody.append(text);
+				panelBody.append(link);
+	        if(statusTxt == "error")
+	            console.log("Error: " + xhr.status + ": " + xhr.statusText);
+	    });
 	    // Fixes screen scrolling issue for big selections
 	    var selected = $(this);
 	    var collapseh = $(".collapse.in").height();
